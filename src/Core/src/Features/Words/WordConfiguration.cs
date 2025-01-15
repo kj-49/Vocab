70cef0c4c7e4374c74df2648f5abdb620a378f8f
kj-49 <ksjames49@gmail.com>
2025-01-14 23:22:56 -0700

    fix: add missing word configuration

---
+++ src/Core/src/Features/Words/WordConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocab.Core.Features.Words;
+public class WordConfiguration : IEntityTypeConfiguration<Word>
+{
+    public void Configure(EntityTypeBuilder<Word> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.HasOne(u => u.Language)
+            .WithMany()
+            .HasForeignKey(u => u.LanguageId);
+    }
+}
