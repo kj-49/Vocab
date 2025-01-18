bb61bb0bbf27a475bd08a6f15d635f799747c526
kj-49 <ksjames49@gmail.com>
2025-01-18 10:34:02 -0700

    feat: seed roles in database

--- src/Core/src/Features/Identity/Roles/AppRoleConfiguration.cs
+++ src/Core/src/Features/Identity/Roles/AppRoleConfiguration.cs
@@ -12,5 +12,25 @@ public class AppRoleConfiguration : IEntityTypeConfiguration<AppRole>
     public void Configure(EntityTypeBuilder<AppRole> builder)
     {
         builder.ToTable("asp_net_roles");
+        builder.HasData(
+            new AppRole
+            {
+                Id = Guid.NewGuid(),
+                Name = "Master",
+                NormalizedName = "MASTER"
+            },
+            new AppRole
+            {
+                Id = Guid.NewGuid(),
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new AppRole
+            {
+                Id = Guid.NewGuid(),
+                Name = "Client",
+                NormalizedName = "CLIENT"
+            }
+        );
     }
 }
---
+++ src/Core/src/Migrations/20250118173223_SeedRoles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Vocab.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "asp_net_roles",
+                columns: new[] { "id", "concurrency_stamp", "name", "normalized_name" },
+                values: new object[,]
+                {
+                    { new Guid("0fc9654a-1bec-4593-b0c6-0bb845630e1f"), null, "Admin", "ADMIN" },
+                    { new Guid("5554c76e-542c-4994-907f-fde29f8e4fd9"), null, "Client", "CLIENT" },
+                    { new Guid("c4dd2d51-dd9f-495a-bd22-670c0f77ec3e"), null, "Master", "MASTER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "asp_net_roles",
+                keyColumn: "id",
+                keyValue: new Guid("0fc9654a-1bec-4593-b0c6-0bb845630e1f"));
+
+            migrationBuilder.DeleteData(
+                table: "asp_net_roles",
+                keyColumn: "id",
+                keyValue: new Guid("5554c76e-542c-4994-907f-fde29f8e4fd9"));
+
+            migrationBuilder.DeleteData(
+                table: "asp_net_roles",
+                keyColumn: "id",
+                keyValue: new Guid("c4dd2d51-dd9f-495a-bd22-670c0f77ec3e"));
+        }
+    }
+}
